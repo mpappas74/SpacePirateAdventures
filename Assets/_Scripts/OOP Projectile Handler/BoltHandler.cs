46c70880b22a455a14ee8c9355292ec82498abc6
Mike Pappas <mpappas86@gmail.com>
2014-02-06 15:31:51 -0500

    Made points slowly recover at a variable rate and added dimming of laser shots.

--- Assets/_Scripts/OOP Projectile Handler/BoltHandler.cs
+++ Assets/_Scripts/OOP Projectile Handler/BoltHandler.cs
@@ -5,8 +5,9 @@ public class BoltHandler : ProjectileHandler {
 
 	// Use this for initialization
 	public override void Start () {
-			base.Start();
-			doesSingleShotDamage = true;
+		fadesAway = true;
+		doesSingleShotDamage = true;
+		base.Start();
 	}
 	
 	// Update is called once per frame
