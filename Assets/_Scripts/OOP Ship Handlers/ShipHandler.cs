fbdba83049630f4ce6a65cbb4a0aca7f2cf8d968
Mike Pappas <mpappas86@gmail.com>
2014-03-01 17:12:35 -0500

    Made all ships moving on iTween paths bounce off of other ships instead of just plowing through them. Looks ugly but we can work on it.

--- Assets/_Scripts/OOP Ship Handlers/ShipHandler.cs
+++ Assets/_Scripts/OOP Ship Handlers/ShipHandler.cs
@@ -128,10 +128,10 @@ public class ShipHandler : MonoBehaviour
 			
 			//If we don't turn around on collisions, then collide with the other ship. The above isItAnEnemyAndI'mNot logic is to keep from double subtracting accidentally.
 			//A collision results in both ships taking damage equal to the weaker one's health.
-			
-			float damage = Mathf.Min (shipHealth, other.gameObject.GetComponent<ShipHandler> ().shipHealth);
-			DecreaseHealth (damage);
-			other.gameObject.GetComponent<ShipHandler> ().DecreaseHealth (damage);
+			GetComponent<iTween>().BackAndThenKeepGoing(0.2f, 0.1f);
+			//float damage = Mathf.Min (shipHealth, other.gameObject.GetComponent<ShipHandler> ().shipHealth);
+			//DecreaseHealth (damage);
+			//other.gameObject.GetComponent<ShipHandler> ().DecreaseHealth (damage);
 		}
 	}
 	
